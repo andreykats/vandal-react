2c8b6d640347c768e8ffa8c4cb662eb47356d44b
crispygoat <121363841+crispygoat@users.noreply.github.com>
2022-12-26 16:16:46 -0500

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
-export const API_URL = "http://0.0.0.0:8000/";
+// export const API_URL = "http://0.0.0.0:8000/";
+export const API_URL = "http://aebc-2603-7000-aa03-db6b-647b-c40d-9fb9-83e.ngrok.io/";
 // export const API_URL = "./";
 
 
